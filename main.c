07b2fa3c2f1c0177c1574eb47cab33ff41889db6
Takeru Osawa <st14d07@gm.ibaraki-ct.ac.jp>
2016-10-13 15:37:24 +0900

    shoya

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 
 int main(void) {
 	int payments[num];
+	int earning[num];
 	int i;
 	for(i = 0; i < num; i++) {
+		printf("payments[%d]: ", i+1);
 		scanf("%d", &payments[i]);
+		printf("earning[%d]: ", i+1);
+		scanf("%d", &earning[i]);
 	}
 	return 0;
 }
