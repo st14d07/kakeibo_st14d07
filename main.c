3ce800bb6c4f4a0e3f3ace356b45b38401b82596
Takeru Osawa <st14d07@gm.ibaraki-ct.ac.jp>
2016-10-20 16:22:39 +0900

    complete

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "average.c"
 
 int main(void) {
-	int payments[5] = {2,4,6,7,8}
+	int payments[5] = {2,4,6,7,8};
 	printf("合計: %d\n平均: %d\n", sum(payments), average(payments));
 	return 0;
 }
